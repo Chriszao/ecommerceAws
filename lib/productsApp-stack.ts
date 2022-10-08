8d811ccfea01a47585b79bf49262dca7f361d183
Chrizao <chris.f.assis18@gmail.com>
2022-10-08 15:30:37 -0300

    feat: Add dynamo table and adminProducts lambda

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -1,14 +1,30 @@
 import type { StackProps } from 'aws-cdk-lib';
-import { Duration, Stack } from 'aws-cdk-lib';
-import {} from 'aws-cdk-lib/aws-lambda';
+import { RemovalPolicy, Duration, Stack } from 'aws-cdk-lib';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import type { Construct } from 'constructs';
 
 export class ProductsAppStack extends Stack {
   readonly fetchProductsHandler: NodejsFunction;
 
+  readonly adminProductsHandler: NodejsFunction;
+
+  readonly productsDynamoDb: Table;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    this.productsDynamoDb = new Table(this, 'ProductsDynamoDb', {
+      tableName: 'products',
+      removalPolicy: RemovalPolicy.DESTROY,
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING,
+      },
+      billingMode: BillingMode.PROVISIONED,
+      readCapacity: 1,
+      writeCapacity: 1,
+    });
 
     this.fetchProductsHandler = new NodejsFunction(this, 'fetchProducts', {
       functionName: 'fetchProducts',
@@ -20,6 +36,28 @@ export class ProductsAppStack extends Stack {
         minify: true,
         sourceMap: false,
       },
+      environment: {
+        PRODUCTS_DYNAMO_DB: this.productsDynamoDb.tableName,
+      },
     });
+
+    this.productsDynamoDb.grantReadData(this.fetchProductsHandler);
+
+    this.adminProductsHandler = new NodejsFunction(this, 'adminProducts', {
+      functionName: 'adminProducts',
+      entry: 'lambda/products/adminProducts.ts',
+      handler: 'handler',
+      memorySize: 128,
+      timeout: Duration.seconds(6),
+      bundling: {
+        minify: true,
+        sourceMap: false,
+      },
+      environment: {
+        PRODUCTS_DYNAMO_DB: this.productsDynamoDb.tableName,
+      },
+    });
+
+    this.productsDynamoDb.grantWriteData(this.adminProductsHandler);
   }
 }
