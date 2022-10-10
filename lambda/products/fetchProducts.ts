cc9a2358cb87cfcbb89fd6a64b81068ea604580c
Chrizao <chris.f.assis18@gmail.com>
2022-10-10 20:03:01 -0300

    feat: Get ProductsRepository into fetch products lambda to fetch products

--- lambda/products/fetchProducts.ts
+++ lambda/products/fetchProducts.ts
@@ -3,37 +3,77 @@ import type {
   APIGatewayProxyResult,
   Context,
 } from 'aws-lambda';
+import { DynamoDB } from 'aws-sdk';
+
+import { ProductRepository } from '/opt/nodejs/productsLayer';
+
+const tableName = process.env.DYNAMO_TABLE_NAME as string;
+const dynamoDbClient = new DynamoDB.DocumentClient();
+const productsRepository = new ProductRepository(dynamoDbClient, tableName);
 
 export async function handler(
   event: APIGatewayProxyEvent,
   context: Context,
 ): Promise<APIGatewayProxyResult> {
-  const { httpMethod, resource, requestContext } = event;
+  try {
+    const { httpMethod, resource, requestContext } = event;
 
-  const { awsRequestId } = context;
-  const { requestId } = requestContext;
+    const { awsRequestId } = context;
+    const { requestId } = requestContext;
 
-  console.log(
-    `API Gateway RequestId: ${awsRequestId} - Lambda RequestId: ${requestId}`,
-  );
+    console.log(
+      `API Gateway RequestId: ${awsRequestId} - Lambda RequestId: ${requestId}`,
+    );
 
-  if (resource === '/products') {
-    if (httpMethod === 'GET') {
-      console.log('#EVENT', event);
+    if (resource === '/products') {
+      if (httpMethod === 'GET') {
+        const products = await productsRepository.fetchProducts();
+
+        return {
+          statusCode: 200,
+          body: JSON.stringify({
+            products,
+          }),
+        };
+      }
+    }
+
+    if (resource === '/products/{id}') {
+      const productId = event.pathParameters?.id as string;
+
+      const product = await productsRepository.getProductById(productId);
+
+      if (!product) {
+        return {
+          statusCode: 404,
+          body: JSON.stringify({
+            message: 'Product not found!',
+          }),
+        };
+      }
 
       return {
         statusCode: 200,
         body: JSON.stringify({
-          message: 'Hello World',
+          product,
         }),
       };
     }
-  }
 
-  return {
-    statusCode: 400,
-    body: JSON.stringify({
-      message: 'Bad request',
-    }),
-  };
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'Bad request',
+      }),
+    };
+  } catch (error) {
+    console.error((<Error>error).message);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: (<Error>error).message ?? 'Internal error!',
+      }),
+    };
+  }
 }
