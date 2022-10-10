228a5d8096b39096d6c7a3e718141a951318262d
Chrizao <chris.f.assis18@gmail.com>
2022-10-10 20:01:34 -0300

    feat: Add ProductsAppLayers

---
+++ lib/productsAppLayer-stack.ts
@@ -0,0 +1,25 @@
+import type { StackProps } from 'aws-cdk-lib';
+import { RemovalPolicy, Stack } from 'aws-cdk-lib';
+import { Code, LayerVersion, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { StringParameter } from 'aws-cdk-lib/aws-ssm';
+import type { Construct } from 'constructs';
+
+export class ProductsAppLayersStack extends Stack {
+  readonly productsLayer: LayerVersion;
+
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    this.productsLayer = new LayerVersion(this, 'ProductsLayer', {
+      code: Code.fromAsset('lambda/products/layers/productsLayer'),
+      compatibleRuntimes: [Runtime.NODEJS_14_X],
+      layerVersionName: 'ProductsLayer',
+      removalPolicy: RemovalPolicy.RETAIN,
+    });
+
+    new StringParameter(this, 'ProductsLayerVersionArn', {
+      parameterName: 'ProductsLayerVersionArn',
+      stringValue: this.productsLayer.layerVersionArn,
+    });
+  }
+}
